71783aea45c87cea08e9d09050243693d540b359
lana-cnmd <133262813+lana-cnmd@users.noreply.github.com>
2023-11-17 23:01:39 +0300

    constructor copy

--- s21_matrix_oop.cc
+++ s21_matrix_oop.cc
@@ -1,23 +1,42 @@
 #include "s21_matrix_oop.h"
 
-S21Matrix::S21Matrix() : rows_(0), cols_(0), matrix_(nullptr){}
+S21Matrix::S21Matrix() : rows_(0), cols_(0), matrix_(nullptr) {}
 
-S21Matrix::S21Matrix(size_t rows, size_t cols) : rows_(rows), cols_(cols) {
-    matrix_ = new double*[rows];
-    for (size_t i = 0; i < rows; ++i) {
+S21Matrix::S21Matrix(size_t rows, size_t cols) : rows_(rows), cols_(cols)
+{
+    matrix_ = new double *[rows];
+    for (size_t i = 0; i < rows; ++i)
+    {
         matrix_[i] = new double[cols]();
     }
 }
 
-// S21Matrix::S21Matrix(const S21Matrix& other) : {}
+S21Matrix::S21Matrix(const S21Matrix &other) :
+{ // почему-то ошибка
+    this->rows_ = other.rows_;
+    this->cols_ = other.cols_;
+    this->matrix_ = new double *[other.rows_];
+    for (size_t i = 0; i < other.rows_; ++i)
+    {
+        this->matrix_[i] = new double[other.cols_]();
+    }
+
+    for (size_t i = 0; i < other.rows_; ++i)
+    {
+        for (size_t j = 0; j < other.cols_; ++j)
+        {
+            this->matrix_[i][j] = other.matrix_[i][j];
+        }
+    }
+}
 
 // S21Matrix::S21Matrix(S21Matrix&& other) : {}
 
-S21Matrix::~S21Matrix() {
-    for (size_t i = 0; i < rows_; ++i) {
-        delete[] matrix_;
+S21Matrix::~S21Matrix()
+{
+    for (size_t i = 0; i < rows_; ++i)
+    {
+        delete[] matrix_[i];
     }
     delete matrix_;
-    cols_ = 0; // для чего?
-    rows_ = 0; // для чего?
 }
--- s21_matrix_oop.h
+++ s21_matrix_oop.h
@@ -3,28 +3,29 @@
 
 #include <iostream>
 
-class S21Matrix {
-    public:
-        S21Matrix();
-        S21Matrix(size_t rows, size_t cols);
-        // S21Matrix(const S21Matrix& other);
-        // S21Matrix(S21Matrix&& other);
-        ~S21Matrix();
+class S21Matrix
+{
+public:
+    S21Matrix();
+    S21Matrix(size_t rows, size_t cols);
+    S21Matrix(const S21Matrix &other);
+    // S21Matrix(S21Matrix&& other);
+    ~S21Matrix();
 
-        // bool EqMatrix(const S21Matrix& other);
-        // void SumMatrix(const S21Matrix& other);
-        // void SubMatrix(const S21Matrix& other);
-        // void MulNumber(const double num);
-        // void MulMatrix(const S21Matrix& other);
-        // S21Matrix Transpose();
-        // S21Matrix CalcComplements();
-        // double Determinant();
-        // S21Matrix InverseMatrix();
+    // bool EqMatrix(const S21Matrix& other);
+    // void SumMatrix(const S21Matrix& other);
+    // void SubMatrix(const S21Matrix& other);
+    // void MulNumber(const double num);
+    // void MulMatrix(const S21Matrix& other);
+    // S21Matrix Transpose();
+    // S21Matrix CalcComplements();
+    // double Determinant();
+    // S21Matrix InverseMatrix();
 
-    private:
-        size_t rows_;
-        size_t cols_;
-        double **matrix_;
+private:
+    size_t rows_;
+    size_t cols_;
+    double **matrix_;
 };
 
-#endif //CPP1_S21_MATRIXPLUS_1_S21_MATRIX_OOP_H
+#endif // CPP1_S21_MATRIXPLUS_1_S21_MATRIX_OOP_H
